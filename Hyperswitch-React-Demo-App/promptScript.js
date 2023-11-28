8be15ade745dba942b95cfb979f6af104208c62b
Praful Koppalkar <126236898+prafulkoppalkar@users.noreply.github.com>
2023-11-28 16:09:05 +0530

    fix: HS-444: Updated prompt script (#45)

--- Hyperswitch-React-Demo-App/promptScript.js
+++ Hyperswitch-React-Demo-App/promptScript.js
@@ -10,48 +10,45 @@ const appClientURL = prompt("Application Client URL : ");
 
 const envPath = "./.env";
 
-const publishableKeyDesc = "Publishable key added";
-const secretKeyDesc = "Secret key added";
-const serverURLDesc = "Self-hosted Hyperswitch Server URL added";
-const clientURLDesc = "Self-hosted Hyperswitch Client URL added";
-const appServerURLDesc = "Application Server URL added";
-const appClientURLDesc = "Application Client URL added";
+const obj = {
+  HYPERSWITCH_PUBLISHABLE_KEY: publishableKey,
+  HYPERSWITCH_SECRET_KEY: secretKey,
+  HYPERSWITCH_SERVER_URL: serverURL,
+  HYPERSWITCH_CLIENT_URL: clientURL,
+  SELF_SERVER_URL: appServerURL,
+  SELF_CLIENT_URL: appClientURL,
+};
 
-function replace(filePath, oldLine, newLine, desc) {
+function initializeValues(filePath, keyValuePairs) {
   try {
-    // Step 1: Read the file
     let data = fs.readFileSync(filePath, "utf8");
 
-    // Step 2: Replace the line
-    data = data.replaceAll(oldLine, newLine);
+    // Split the content by newline to operate on individual lines
+    let lines = data.split("\n");
 
-    // Step 3: Write the updated content back to the file
+    // Process each key-value pair
+    Object.entries(keyValuePairs).forEach(([key, value]) => {
+      // Find if the key already exists in the .env file
+      let index = lines.findIndex((line) => line.startsWith(`${key}=`));
+
+      if (index !== -1) {
+        // If the key exists, update its value
+        lines[index] = `${key}="${value}"`;
+      } else {
+        // If the key doesn't exist, add a new line with the key-value pair
+        lines.push(`${key}="${value}"`);
+      }
+    });
+
+    // Join the lines back together
+    data = lines.join("\n");
+
+    // Write the updated content back to the file
     fs.writeFileSync(filePath, data);
 
-    console.log(`${desc} successfully.`);
+    console.log("Initialization of values successful.");
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
 }
-replace(envPath, "SELF_HOSTED_CLIENT_URL", clientURL, clientURLDesc);
-replace(
-  envPath,
-  "ENTER_YOUR_CLIENT_APPLICATION_URL",
-  appClientURL,
-  appClientURLDesc
-);
-replace(
-  envPath,
-  "ENTER_YOUR_SERVER_APPLICATION_URL",
-  appServerURL,
-  appServerURLDesc
-);
-
-replace(
-  envPath,
-  "GET_PUBLISHABLE_KEY_FROM_DASHBOARD",
-  publishableKey,
-  publishableKeyDesc
-);
-replace(envPath, "GET_SECRET_KEY_FROM_DASHBOARD", secretKey, secretKeyDesc);
-replace(envPath, "SELF_HOSTED_SERVER_URL", serverURL, serverURLDesc);
+initializeValues(envPath, obj);
