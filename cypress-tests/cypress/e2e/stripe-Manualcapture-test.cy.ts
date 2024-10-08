33a37a3389255191e7e0db8d67e583e5e6f84108
preetamrevankar <preetam.revankar@juspay.in>
2024-10-08 19:24:58 +0530

    feat: added test cases in cypress

---
+++ cypress-tests/cypress/e2e/stripe-Manualcapture-test.cy.ts
@@ -0,0 +1,48 @@
+import * as testIds from "../../../src/Utilities/TestUtils.bs";
+import { getClientURL } from "../support/utils";
+import { createPaymentBody } from "../support/utils";
+import { changeObjectKeyValue } from "../support/utils";
+import { stripeCards } from "cypress/support/cards";
+
+describe("Card payment flow test", () => {
+
+  const publishableKey = Cypress.env('HYPERSWITCH_PUBLISHABLE_KEY')
+  const secretKey = Cypress.env('HYPERSWITCH_SECRET_KEY')
+  let getIframeBody: () => Cypress.Chainable<JQuery<HTMLBodyElement>>;
+  let iframeSelector =
+    "#orca-payment-element-iframeRef-orca-elements-payment-element-payment-element";
+
+   changeObjectKeyValue(createPaymentBody,"capture_method","manual")
+
+
+  beforeEach(() => {
+    getIframeBody = () => cy.iframe(iframeSelector);
+    cy.createPaymentIntent(secretKey, createPaymentBody).then(() => {
+      cy.getGlobalState("clientSecret").then((clientSecret) => {
+
+        cy.visit(getClientURL(clientSecret, publishableKey));
+      });
+
+    })
+  });
+
+it("should complete the card payment successfully", () => {
+  
+    const { cardNo, card_exp_month, card_exp_year, cvc } = stripeCards.successCard;
+
+    // Wait for iframe to load and get its body
+    getIframeBody().find('[data-testid=cardNoInput]').type(cardNo); // Example card number
+    getIframeBody().find('[data-testid=expiryInput]').type(card_exp_month); // Expiration month
+    getIframeBody().find('[data-testid=expiryInput]').type(card_exp_year); // Expiration year
+    getIframeBody().find('[data-testid=cvvInput]').type(cvc); // CVV
+
+    // Click on the submit button
+    getIframeBody().get("#submit").click();
+
+     // Wait for the response and assert payment processing message
+  cy.wait(3000);
+  cy.contains('Payment processing! Requires manual capture')
+  .should('be.visible');
+
+});
+});
