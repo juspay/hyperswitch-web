f51614ebfdd7f9f56de534ac5901a8b975e18526
aritro.ghosh <aritro.ghosh@juspay.in>
2024-12-05 16:15:49 +0530

    test: test card payment with multiple card brands

---
+++ cypress-tests/cypress/e2e/multiple-card-test.cy.ts
@@ -0,0 +1,88 @@
+import * as testIds from "../../../src/Utilities/TestUtils.bs";
+import { getClientURL } from "../support/utils";
+import { createPaymentBody } from "../support/utils";
+import { changeObjectKeyValue } from "../support/utils";
+import {  stripeCards } from "cypress/support/cards";
+
+describe("Card number validation test", () => {
+
+    const publishableKey = Cypress.env('HYPERSWITCH_PUBLISHABLE_KEY')
+    const secretKey = Cypress.env('HYPERSWITCH_SECRET_KEY')
+    let getIframeBody: () => Cypress.Chainable<JQuery<HTMLBodyElement>>;
+    let iframeSelector =
+        "#orca-payment-element-iframeRef-orca-elements-payment-element-payment-element";
+
+    // changeObjectKeyValue(createPaymentBody,"profile_id","YOUR_PROFILE_ID")
+     changeObjectKeyValue(createPaymentBody,"customer_id","new_user")
+
+
+    beforeEach(() => {
+        getIframeBody = () => cy.iframe(iframeSelector);
+        cy.createPaymentIntent(secretKey, createPaymentBody).then(() => {
+            cy.getGlobalState("clientSecret").then((clientSecret) => {
+
+                cy.visit(getClientURL(clientSecret, publishableKey));
+            });
+
+        })
+    });
+
+
+    it("19 digit visa card", () => {
+        const { cardNo, card_exp_month, card_exp_year, cvc } = stripeCards.visaCard19;
+    
+        getIframeBody().find(`[data-testid=${testIds.cardNoInputTestId}]`).type(cardNo); 
+        getIframeBody().find(`[data-testid=${testIds.expiryInputTestId}]`).type(card_exp_month); 
+        getIframeBody().find(`[data-testid=${testIds.expiryInputTestId}]`).type(card_exp_year); 
+        getIframeBody().find(`[data-testid=${testIds.cardCVVInputTestId}]`).type(cvc); 
+    
+        getIframeBody().get("#submit").click();
+    
+        
+        cy.contains("Thanks for your order!").should("be.visible");
+      });
+
+
+      it("16 digit master card", () => {
+        const { cardNo, card_exp_month, card_exp_year, cvc } = stripeCards.masterCard16;
+    
+        getIframeBody().find(`[data-testid=${testIds.cardNoInputTestId}]`).type(cardNo); 
+        getIframeBody().find(`[data-testid=${testIds.expiryInputTestId}]`).type(card_exp_month); 
+        getIframeBody().find(`[data-testid=${testIds.expiryInputTestId}]`).type(card_exp_year); 
+        getIframeBody().find(`[data-testid=${testIds.cardCVVInputTestId}]`).type(cvc); 
+    
+        getIframeBody().get("#submit").click();
+    
+        
+        cy.contains("Thanks for your order!").should("be.visible");
+      });
+
+      it("15 digit american express card", () => {
+        const { cardNo, card_exp_month, card_exp_year, cvc } = stripeCards.amexCard15;
+    
+        getIframeBody().find(`[data-testid=${testIds.cardNoInputTestId}]`).type(cardNo); 
+        getIframeBody().find(`[data-testid=${testIds.expiryInputTestId}]`).type(card_exp_month); 
+        getIframeBody().find(`[data-testid=${testIds.expiryInputTestId}]`).type(card_exp_year); 
+        getIframeBody().find(`[data-testid=${testIds.cardCVVInputTestId}]`).type(cvc); 
+    
+        getIframeBody().get("#submit").click();
+    
+        
+        cy.contains("Thanks for your order!").should("be.visible");
+      });
+
+      it("14 digit diners club card", () => {
+        const { cardNo, card_exp_month, card_exp_year, cvc } = stripeCards.dinersClubCard14;
+    
+        getIframeBody().find(`[data-testid=${testIds.cardNoInputTestId}]`).type(cardNo); 
+        getIframeBody().find(`[data-testid=${testIds.expiryInputTestId}]`).type(card_exp_month); 
+        getIframeBody().find(`[data-testid=${testIds.expiryInputTestId}]`).type(card_exp_year); 
+        getIframeBody().find(`[data-testid=${testIds.cardCVVInputTestId}]`).type(cvc); 
+    
+        getIframeBody().get("#submit").click();
+    
+        
+        cy.contains("Thanks for your order!").should("be.visible");
+      });
+
+});
