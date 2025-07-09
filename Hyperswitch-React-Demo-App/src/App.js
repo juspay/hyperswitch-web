ad84ee568d75c115481ce67584c78884b6c90948
Pritish Budhiraja <pritish.budhiraja@gmail.com>
2025-07-09 15:58:38 +0530

    fix: warning removed

--- Hyperswitch-React-Demo-App/src/App.js
+++ Hyperswitch-React-Demo-App/src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Payment from "./Payment";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
       <Routes>
         <Route path="/" element={<Payment />} />
       </Routes>
